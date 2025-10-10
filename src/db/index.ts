63546834ba8e0a9c22adbeba2bb85647855636fc
Mate Mišlov <matemislov90@gmail.com>
2025-10-10 15:22:36 +0200

    Refactor package.json scripts to remove hardcoded MongoDB environment variable and update database connection URL in index.ts to use TURSO_DATABASE_URL, enhancing configuration flexibility.

--- src/db/index.ts
+++ src/db/index.ts
@@ -4,7 +4,7 @@ import { drizzle } from 'drizzle-orm/libsql'
 // You can specify any property from the libsql connection options
 const db = drizzle({
   connection: {
-    url: process.env.DATABASE_URI!,
+    url: process.env.TURSO_DATABASE_URL!,
     authToken: process.env.TURSO_AUTH_TOKEN!,
   },
 })
